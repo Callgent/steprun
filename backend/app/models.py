b34c0abe5ecf1446e1347b150ea81f56f7cae108
dev-callgent <dev@callgent.com>
2025-05-21 08:08:55 +0800

    feat: user.created_at

--- backend/app/models.py
+++ backend/app/models.py
@@ -49,6 +49,7 @@ class UpdatePassword(SQLModel):
 # Database model, database table inferred from class name
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), index=True)
     hashed_password: str
     api_key: str | None = Field(default=None, unique=True, index=True)
     items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
